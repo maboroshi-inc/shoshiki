52cf50a75955cb1f418b65e4137f0c7dca8388cd
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-09-10 14:58:11 +0900

    create the API document.

---
+++ docs/assets/js/search.js
@@ -0,0 +1,21 @@
+var typedoc = typedoc || {}
+typedoc.search = typedoc.search || {}
+typedoc.search.data = {
+  kinds: { '64': 'Function' },
+  rows: [
+    {
+      id: 0,
+      kind: 64,
+      name: 'nlToBr',
+      url: 'globals.html#nltobr',
+      classes: 'tsd-kind-function'
+    },
+    {
+      id: 1,
+      kind: 64,
+      name: 'numToCommaSeparated',
+      url: 'globals.html#numtocommaseparated',
+      classes: 'tsd-kind-function'
+    }
+  ]
+}
