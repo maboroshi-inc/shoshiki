3ff7fb9bd3457af30588ebae740b8835e604ce08
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-09-14 15:19:18 +0900

    APIドキュメントを更新した。

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,21 +1,3 @@
-var typedoc = typedoc || {}
-typedoc.search = typedoc.search || {}
-typedoc.search.data = {
-  kinds: { '64': 'Function' },
-  rows: [
-    {
-      id: 0,
-      kind: 64,
-      name: 'nlToBr',
-      url: 'globals.html#nltobr',
-      classes: 'tsd-kind-function'
-    },
-    {
-      id: 1,
-      kind: 64,
-      name: 'numToCommaSeparated',
-      url: 'globals.html#numtocommaseparated',
-      classes: 'tsd-kind-function'
-    }
-  ]
-}
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"isEMailAddress","url":"globals.html#isemailaddress","classes":"tsd-kind-function"},{"id":1,"kind":64,"name":"isJapanesePhoneNumberWithoutHyphens","url":"globals.html#isjapanesephonenumberwithouthyphens","classes":"tsd-kind-function"},{"id":2,"kind":64,"name":"isJapanesePhoneNumber","url":"globals.html#isjapanesephonenumber","classes":"tsd-kind-function"},{"id":3,"kind":64,"name":"nlToBr","url":"globals.html#nltobr","classes":"tsd-kind-function"},{"id":4,"kind":64,"name":"numToCommaSeparated","url":"globals.html#numtocommaseparated","classes":"tsd-kind-function"}]};
