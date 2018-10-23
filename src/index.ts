e14c20236c2b620d6fa458c77cc93bc89c139374
KIMURA Tetsuro <haribote.nobody@gmail.com>
2018-10-23 12:49:00 +0900

    メソッド名で入出力が全角であることを明示した。

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export { hiraganaToKatakana } from './hiraganaToKatakana'
+export { hiraganaToZenkakuKatakana } from './hiraganaToZenkakuKatakana'
 export { isEMailAddress } from './isEMailAdrress'
 export { isJapanesePhoneNumber } from './isJapanesePhoneNumber'
 export {
   isJapanesePhoneNumberWithoutHyphens
 } from './isJapanesePhoneNumberWithoutHyphens'
-export { katakanaToHiragana } from './katakanaToHiragana'
 export { nlToBr } from './nlToBr'
 export { numToCommaSeparated } from './numToCommaSeparated'
 export {
   zenkakuAlphaNumSymbolToHankaku
 } from './zenkakuAlphaNumSymbolToHankaku'
+export { zenkakuKatakanaToHiragana } from './zenkakuKatakanaToHiragana'
