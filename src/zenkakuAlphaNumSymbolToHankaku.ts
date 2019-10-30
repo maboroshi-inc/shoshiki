a058604d981ed3963e64d81781ebd3297ba823f1
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-30 18:40:21 +0900

    `zenkakuAlphaNumSymbolToHankaku()` 関数の戻り値型を定義した。

--- src/zenkakuAlphaNumSymbolToHankaku.ts
+++ src/zenkakuAlphaNumSymbolToHankaku.ts
@@ -2,7 +2,7 @@
  * 全角英数記号を半角に変換する
  * @param str 変換対象文字列
  */
-export function zenkakuAlphaNumSymbolToHankaku(str: string) {
+export function zenkakuAlphaNumSymbolToHankaku(str: string): string {
   return str
     .replace(/[！-～]/g, match => {
       const char = match.charCodeAt(0) - 0xfee0
