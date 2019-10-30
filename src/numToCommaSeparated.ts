b93b88ed6d2417c7145a9b98770873b433c7a371
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-30 18:39:43 +0900

    `numToCommaSeparated()` 関数の戻り値型を定義した。

--- src/numToCommaSeparated.ts
+++ src/numToCommaSeparated.ts
@@ -2,7 +2,7 @@
  * 数値をカンマ区切りの文字列に整形する
  * @param num 整形したい数値
  */
-export function numToCommaSeparated(num: number) {
+export function numToCommaSeparated(num: number): string {
   return num
     .toString()
     .replace(/(\d)(?=(?:\d{3}){2,}(?:\.|$))|(\d)(\d{3}(?:\.\d*)?$)/g, '$1$2,$3')
