343df4c63de4384a5c4450778e64f73b99cb1834
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-30 18:45:48 +0900

    `zenkakuAlphaNumSymbolToHankaku()` 関数の処理中に必要な全角スペースを例外として許可するようにした。

--- src/zenkakuAlphaNumSymbolToHankaku.ts
+++ src/zenkakuAlphaNumSymbolToHankaku.ts
@@ -3,15 +3,19 @@
  * @param str 変換対象文字列
  */
 export function zenkakuAlphaNumSymbolToHankaku(str: string): string {
-  return str
-    .replace(/[！-～]/g, match => {
-      const char = match.charCodeAt(0) - 0xfee0
-      return String.fromCharCode(char)
-    })
-    .replace(/”/g, '"')
-    .replace(/’/g, "'")
-    .replace(/‘/g, '`')
-    .replace(/￥/g, '\\')
-    .replace(/　/g, ' ')
-    .replace(/〜/g, '~')
+  return (
+    str
+      .replace(/[！-～]/g, match => {
+        const char = match.charCodeAt(0) - 0xfee0
+        return String.fromCharCode(char)
+      })
+      .replace(/”/g, '"')
+      .replace(/’/g, "'")
+      .replace(/‘/g, '`')
+      .replace(/￥/g, '\\')
+      // 全角スペースを例外として許可する
+      // eslint-disable-next-line no-irregular-whitespace
+      .replace(/　/g, ' ')
+      .replace(/〜/g, '~')
+  )
 }
