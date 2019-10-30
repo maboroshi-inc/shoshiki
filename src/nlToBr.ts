e5b60da849dc9c85bfbe253e5c775b783dc84278
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-30 18:21:49 +0900

    `nlToBr()` 関数の戻り値型を定義した。

--- src/nlToBr.ts
+++ src/nlToBr.ts
@@ -3,6 +3,6 @@
  * @param str 整形したい文字列
  * @param keepLine 改行コードを残すか否か
  */
-export function nlToBr(str: string, keepLine = false) {
+export function nlToBr(str: string, keepLine = false): string {
   return str.replace(/(\x0D\x0A|\x0d|\x0A)/g, `<br>${keepLine ? '$1' : ''}`)
 }
