14acd27e84b50b9a41fa826a834d0f076ef01d29
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-10-30 18:16:33 +0900

    `isEMailAddress()` 関数の戻り値型を定義した。

--- src/isEMailAdrress.ts
+++ src/isEMailAdrress.ts
@@ -4,7 +4,7 @@
  * @see {@link https://github.com/maboroshi-inc/shoshiki/blob/master/__tests__/isEMailAddress.spec.ts|テストケース}
  * @param str 判定対象文字列
  */
-export function isEMailAddress(str: string) {
+export function isEMailAddress(str: string): boolean {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
     str
   )
