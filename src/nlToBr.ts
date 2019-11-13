088e85368ccc6df8e0bd03b5a4f576d0d75d703f
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-11-13 18:20:54 +0900

    ドキュメントを更新した。

--- src/nlToBr.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 文字列に含まれる改行コードを `<br>` 要素に変換する
- * @param str 整形したい文字列
- * @param keepLine 改行コードを残すか否か
- */
-export function nlToBr(str: string, keepLine = false) {
-  return str.replace(/(\x0D\x0A|\x0d|\x0A)/g, `<br>${keepLine ? '$1' : ''}`)
-}
--- src/numToCommaSeparated.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 数値をカンマ区切りの文字列に整形する
- * @param num 整形したい数値
- */
-export function numToCommaSeparated(num: number) {
-  return num
-    .toString()
-    .replace(/(\d)(?=(?:\d{3}){2,}(?:\.|$))|(\d)(\d{3}(?:\.\d*)?$)/g, '$1$2,$3')
-}
